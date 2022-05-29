37a1a0b4fe7447aaf8858afc63014b9d06098522
Lami <olamide2olowoniyi@gmail.com>
2022-05-29 16:55:35 -0400

    Updates

---
+++ precache-manifest.44f9d991e17fceeb3452f510f1b6eccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a70bb2594ed97605f17",
+    "url": "/robo/static/css/main.78d875e1.chunk.css"
+  },
+  {
+    "revision": "6a70bb2594ed97605f17",
+    "url": "/robo/static/js/main.6a70bb25.chunk.js"
+  },
+  {
+    "revision": "9dafe05b92716cf51d9b",
+    "url": "/robo/static/css/1.edf35531.chunk.css"
+  },
+  {
+    "revision": "9dafe05b92716cf51d9b",
+    "url": "/robo/static/js/1.9dafe05b.chunk.js"
+  },
+  {
+    "revision": "e0336b123eb1f66a9ec9",
+    "url": "/robo/static/js/runtime~main.e0336b12.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6011151d69f828e6c5333f50ed5f697d",
+    "url": "/robo/index.html"
+  }
+];
